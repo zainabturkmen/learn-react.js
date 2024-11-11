c90b6e16b7b7e829a04c629b62839fd2c7aab3c7
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-11 14:50:14 +0430

    updating

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-
 const Navbar = () => {
   return (
     <Wrapper>
       <h1>Navbar</h1>
     </Wrapper>
   );
+  // return <h1>hello</h1>
 };
 
 const Wrapper = styled.div`
-  background-color: red;
+  /* background-color: red; */
 `;
 
 export default Navbar;
