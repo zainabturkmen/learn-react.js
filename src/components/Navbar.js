414e573c8bf06572e396ad3e9ae6e88ae7a34ee9
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-11 14:23:30 +0430

    updating

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div>Navbar</div>;
+};
 
-export default Navbar
+
+
+export default Navbar;
+
